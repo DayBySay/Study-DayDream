efffa1d6b4d1cc26044b79e353327f685d835748
Takayuki Sei <takayuki_sei@voyagegroup.com>
2016-11-08 01:34:46 +0900

    カーソルを動かすスクリプト用意

---
+++ Assets/Scripts/HandController.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HandController : MonoBehaviour {
+	
+	// Update is called once per frame
+	void Update () {
+		this.gameObject.transform.rotation = GvrController.Orientation;
+	}
+}
