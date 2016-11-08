4615eed47d1d5a078affbaf71b11a7d884f065a2
Takayuki Sei <takayuki_sei@voyagegroup.com>
2016-11-08 14:59:39 +0900

    選択したオブジェクトを削除出来るように

--- Assets/Scripts/HandController.cs
+++ Assets/Scripts/HandController.cs
@@ -10,11 +10,26 @@ public class HandController : MonoBehaviour {
 		RaycastHit hitInfo;
 		Vector3 rayDirection = GvrController.Orientation * Vector3.forward;
 //		Debug.DrawRay (transform.position, rayDirection * 10, Color.green); デバッグ時に書いとくと便利
+		GameObject selectedObject = null;
 
 		if (Physics.Raycast (Vector3.zero, rayDirection, out hitInfo)) {
 			if (hitInfo.collider && hitInfo.collider.gameObject) {
-				Debug.Log (hitInfo.collider.gameObject.name);
+				selectedObject = hitInfo.collider.gameObject;
 			}
 		}
+
+		if (selectedObject == null) {
+			return;
+		}
+
+		if (!GvrController.TouchDown) {
+			return;
+		}
+
+		Select (selectedObject);
+	}
+
+	void Select(GameObject selectedObject) {
+		Destroy (selectedObject);
 	}
 }
