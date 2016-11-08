315d83913b6f87c188a3bd7a1716f0087b35013a
Takayuki Sei <takayuki_sei@voyagegroup.com>
2016-11-08 15:17:36 +0900

    説明用にコメントと実装をちょっと変更

--- Assets/Scripts/HandController.cs
+++ Assets/Scripts/HandController.cs
@@ -5,8 +5,10 @@ public class HandController : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
+		// コントローラの傾きをとってカーソルの位置を動かす
 		this.gameObject.transform.rotation = GvrController.Orientation;
 
+		// Rayを使って対象のカーソルに合わさったオブジェクトを取得
 		RaycastHit hitInfo;
 		Vector3 rayDirection = GvrController.Orientation * Vector3.forward;
 //		Debug.DrawRay (transform.position, rayDirection * 10, Color.green); デバッグ時に書いとくと便利
@@ -18,18 +20,9 @@ public class HandController : MonoBehaviour {
 			}
 		}
 
-		if (selectedObject == null) {
-			return;
+		// オブジェクトが選択されている、かつコントローラがタップされている時のみ実行
+		if (selectedObject != null && GvrController.TouchDown) {
+			Destroy (selectedObject);
 		}
-
-		if (!GvrController.TouchDown) {
-			return;
-		}
-
-		Select (selectedObject);
-	}
-
-	void Select(GameObject selectedObject) {
-		Destroy (selectedObject);
 	}
 }
