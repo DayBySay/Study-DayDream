90cff4d8e72362fb13b0d8bd85a267730f9896dd
Takayuki Sei <takayuki_sei@voyagegroup.com>
2016-11-08 14:52:57 +0900

    レイでヒットしたオブジェクトを特定できるようにした

--- Assets/Scripts/HandController.cs
+++ Assets/Scripts/HandController.cs
@@ -6,5 +6,15 @@ public class HandController : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 		this.gameObject.transform.rotation = GvrController.Orientation;
+
+		RaycastHit hitInfo;
+		Vector3 rayDirection = GvrController.Orientation * Vector3.forward;
+//		Debug.DrawRay (transform.position, rayDirection * 10, Color.green); デバッグ時に書いとくと便利
+
+		if (Physics.Raycast (Vector3.zero, rayDirection, out hitInfo)) {
+			if (hitInfo.collider && hitInfo.collider.gameObject) {
+				Debug.Log (hitInfo.collider.gameObject.name);
+			}
+		}
 	}
 }
