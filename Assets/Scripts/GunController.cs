642e8c14a926b900cdbe8b1a940ff6e8b3b6fc7d
Takayuki Sei <takayuki_sei@voyagegroup.com>
2016-11-21 13:59:14 +0900

    銃の追加と全体の位置調整

---
+++ Assets/Scripts/GunController.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GunController : MonoBehaviour {
+	public GameObject gun;
+
+	// Update is called once per frame
+	void Update () {
+		gun.transform.rotation = GvrController.Orientation;
+	}
+}
