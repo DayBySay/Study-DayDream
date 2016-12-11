8d53c8819abd1fbf076f7af75ed63c45a9890fe5
Takayuki Sei <takayuki_sei@voyagegroup.com>
2016-12-11 20:23:21 +0900

    Cubeにスクリプトが反応できるよう実装

---
+++ Assets/Scripts/CubeScript.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CubeScript : MonoBehaviour {
+
+    public void OnPointerEnter() {
+        Debug.Log ("OnPointerEnter");
+    }
+
+    public void OnPointerExit() {
+        Debug.Log ("OnPointerExit");
+    }
+}
